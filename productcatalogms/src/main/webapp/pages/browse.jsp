3a4b492e3837dd5fd588919cd91c4b5f7a8254a2
Hang Yuan <hyuan3@ucsc.edu>
2020-07-09 11:48:56 -0700

    replace the 2.1 for productcatalog

--- productcatalogms/src/main/webapp/pages/browse.jsp
+++
@@ -1,9 +0,0 @@
- 
-  <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    isELIgnored="false" pageEncoding="ISO-8859-1" %>
-<!DOCTYPE HTML>
-<html>
-<body>
-<p>hello world</p>
-</body>
-</html>
---
+++ productcatalogms2.1/src/main/java/com/catalog/service/CartIdGeneratorService.java
@@ -0,0 +1,21 @@
+package com.catalog.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import com.catalog.model.CartIdGenerator;
+
+
+
+
+
+public interface CartIdGeneratorService {
+
+
+public int createCartIdGenerator(CartIdGenerator cartIdGenerator);
+
+
+
+
+}
